16c47aff3ecb2f738bbd380e5301932f880cec27
TIAmo <130211313+ysx88@users.noreply.github.com>
2023-06-20 03:32:25 +0800

    Update diy-r2s.sh

--- configs/diy-r2s.sh
+++ configs/diy-r2s.sh
@@ -18,6 +18,9 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 # 更改 Argon 主题背景
 cp -f $GITHUB_WORKSPACE/configs/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
+# replace banner
+cp -f $GITHUB_WORKSPACE/configs/banner package/base-files/files/etc/banner
+
 # 晶晨宝盒
 sed -i "s|https.*/amlogic-s9xxx-openwrt|https://github.com/ysx88/ysx88|g" package/luci-app-amlogic/root/etc/config/amlogic
 sed -i "s|http.*/library|https://github.com/ysx88/ysx88|g" package/luci-app-amlogic/root/etc/config/amlogic
