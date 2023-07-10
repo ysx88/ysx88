ed292e24a4b8d05da4cee5b6fcafcd1b62d65912
TIAmo <130211313+ysx88@users.noreply.github.com>
2023-07-11 03:31:46 +0800

    Update diy-x86.sh

--- configs/diy-x86.sh
+++ configs/diy-x86.sh
@@ -9,14 +9,6 @@ sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefil
 # Set DISTRIB_REVISION
 sed -i "s/OpenWrt /TIAmo Build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
-# 移除重复软件包
-rm -rf feeds/luci/themes/luci-theme-argon
-
-# Themes
-git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
